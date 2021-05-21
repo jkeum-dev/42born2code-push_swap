be6af84790ee752d6a960bb40ffc16ab41e7b762
JongeunKeum <jon9eun@gmail.com>
2021-05-21 19:59:10 +0900

    modify init_stack.c

--- srcs/init_stack.c
+++ srcs/init_stack.c
@@ -1,38 +1,16 @@
 #include "push_swap.h"
 
-t_stack	*init_node(int idx, int argc, char *argv, t_stack *prev)
-{
-	t_stack	*stack;
-
-	stack = (t_stack *)malloc(sizeof(stack));
-	if (!stack)
-		return (NULL);
-	stack->value = ft_atoi(argv);
-	if (!prev)
-		stack->prev = NULL;
-	else
-	{
-		stack->prev = prev;
-		prev->next = stack;
-	}
-	stack->next = NULL;
-	return (stack);
-}
-
-t_stack	*init_stack(int argc, char **argv)
+t_stack	*make_stack(int argc, char **argv)
 {
 	int		i;
+	t_stack	*temp;
 	t_stack	*stack;
 
 	i = 0;
 	stack = NULL;
 	while (++i < argc)
 	{
-		stack = init_node(i, argc, argv[i], stack);
-		// if (!stack)
-		// 	free_stack(stack);
+		temp = make_node(argc, argv);
+		
 	}
-	while (stack->prev)
-		stack = stack->prev;
-	return (stack);
 }
