a3bba0f91fb4a25b2a798d4878a613048b7e0e5d
JongeunKeum <jon9eun@gmail.com>
2021-05-22 18:59:30 +0900

    change structure name

--- includes/structures.h
+++ includes/structures.h
@@ -1,21 +1,21 @@
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
-typedef struct s_listp	t_listp;
 typedef struct s_stack	t_stack;
+typedef struct s_node	t_node;
 
-struct s_listp
+struct s_stack
 {
 	int		size;
-	t_stack	*top;
-	t_stack	*bottom;
+	t_node	*top;
+	t_node	*bottom;
 };
 
-struct	s_stack
+struct	s_node
 {
 	int		value;
-	t_stack	*prev;
-	t_stack	*next;
+	t_node	*prev;
+	t_node	*next;
 };
 
 #endif
