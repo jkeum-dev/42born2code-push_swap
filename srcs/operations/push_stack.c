4dbc921eac6590613b455331046eeb2d2185ab37
JongeunKeum <jon9eun@gmail.com>
2021-05-24 16:39:02 +0900

    add push_stack.c

---
+++ srcs/operations/push_stack.c
@@ -0,0 +1,56 @@
+#include "push_swap.h"
+
+static void	from_size_one(t_stack *from, t_stack *to)
+{
+	if (to->size == 0)
+	{
+		to->top = from->top;
+		to->bottom = to->top;
+	}
+	else
+	{
+		to->top->prev = from->top;
+		from->top->next = to->top;
+		to->top = to->top->prev;
+	}
+	from->top = NULL;
+	from->bottom = NULL;
+}
+
+static void	push_pop(t_stack *from, t_stack *to)
+{
+	if (from->size == 1)
+		from_size_one(from, to);
+	else
+	{
+		if (to->size == 0)
+		{
+			to->top = from->top;
+			to->bottom = to->top;
+			from->top = from->top->next;
+			from->top->prev = NULL;
+			to->top->next = NULL;
+		}
+		else
+		{
+			to->top->prev = from->top;
+			from->top = from->top->next;
+			from->top->prev = NULL;
+			to->top->prev->next = to->top;
+			to->top = to->top->prev;
+		}
+	}
+	to->size++;
+	from->size--;
+}
+
+void	push_stack(t_stack *from, t_stack *to, int flag)
+{
+	if (from->size == 0)
+		return ;
+	push_pop(from, to);
+	if (flag == A)
+		ft_putendl_fd("pa", 1);
+	else if (flag == B)
+		ft_putendl_fd("pb", 1);
+}
