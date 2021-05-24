03045cad06275ab0211387057327f689c0eb8f1f
JongeunKeum <jon9eun@gmail.com>
2021-05-24 16:17:25 +0900

    add swap functions

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -13,5 +13,7 @@ t_stack	*init_stack(void);
 t_node	*init_node(void);
 t_node	*make_stack(int argc, char **argv, t_stack **stack);
 void	push_pop(t_stack *from, t_stack *to);
+void	swap_stack(t_stack *stack, int flag);
+void	swap_all_stack(t_stack *a, t_stack *b, int flag);
 
 #endif
