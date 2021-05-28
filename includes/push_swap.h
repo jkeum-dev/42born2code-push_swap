51367636f3c2c978eeb2957fdc48ff435daafe28
JongeunKeum <jon9eun@gmail.com>
2021-05-28 21:30:04 +0900

    change and add functions

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -13,8 +13,8 @@ int		ft_atoi(char *arg);
 t_stack	*init_stack(void);
 t_node	*init_node(void);
 t_node	*make_stack(int argc, char **argv, t_stack **stack);
-void	get_max_value(t_stack *stack);
-void	get_big_two_values(t_stack *stack);
+int		get_min_value(t_node *node, int size);
+int		get_max_value(t_node *node, int size);
 
 void	swap_stack(t_stack *stack, int flag);
 void	swap_all_stack(t_stack *a, t_stack *b, int flag);
