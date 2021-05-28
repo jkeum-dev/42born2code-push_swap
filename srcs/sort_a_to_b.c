6cb9c4c129ee2cdf98eb155039603513f2784cee
JongeunKeum <jon9eun@gmail.com>
2021-05-28 22:12:57 +0900

    sort a_to_b and b_to_a

---
+++ srcs/sort_a_to_b.c
@@ -0,0 +1,56 @@
+#include "push_swap.h"
+
+void	a_to_b(int r, t_stack *a, t_stack *b)
+{
+	int		r_temp;
+	int		rrr;
+	int		rem;
+	t_value	*var;
+
+	if (r <= 3)
+		handle_under_three(r, a, b, A);
+	var = init_value();
+	if (!var)
+		return ;
+	select_pivot(r, a, var);
+	r_temp = r;
+	while (r_temp--)
+	{
+		if (a->top->value >= var->piv_big)
+		{
+			rotate_stack(a, A);
+			var->ra++;
+		}
+		else
+		{
+			push_stack(a, b, B);
+			var->pb++;
+			if (b->top->value >= var->piv_small)
+			{
+				rotate_stack(b, B);
+				var->rb++;
+			}
+		}
+	}
+	if (var->ra > var->rb)
+	{
+		rrr = var->rb;
+		rem = var->ra - rrr;
+		while (rrr--)
+			reverse_rotate_all_stack(a, b, ALL);
+		while (rem--)
+			reverse_rotate_stack(a, A);
+	}
+	else
+	{
+		rrr = var->ra;
+		rem = var->rb - rrr;
+		while (rrr--)
+			reverse_rotate_all_stack(a, b, ALL);
+		while (rem--)
+			reverse_rotate_stack(b, B);
+	}
+	a_to_b(var->ra, a, b);
+	b_to_a(var->rb, a, b);
+	b_to_a(var->pb - var->rb, a, b);
+}
---
+++ srcs/sort_b_to_a.c
@@ -0,0 +1,56 @@
+#include "push_swap.h"
+
+void	b_to_a(int r, t_stack *a, t_stack *b)
+{
+	int		r_temp;
+	int		rrr;
+	int		rem;
+	t_value	*var;
+
+	if (r <= 3)
+		handle_under_three(r, a, b, B);
+	var = init_value();
+	if (!var)
+		return ;
+	select_pivot(r, b, var);
+	r_temp = r;
+	while (r_temp--)
+	{
+		if (b->top->value < var->piv_small)
+		{
+			rotate_stack(b, B);
+			var->rb++;
+		}
+		else
+		{
+			push_stack(b, a, A);
+			var->pa++;
+			if (b->top->value < var->piv_big)
+			{
+				rotate_stack(a, A);
+				var->ra++;
+			}
+		}
+	}
+	a_to_b(var->pa - var->ra, a, b);
+	if (var->ra > var->rb)
+	{
+		rrr = var->rb;
+		rem = var->ra - rrr;
+		while (rrr--)
+			reverse_rotate_all_stack(a, b, ALL);
+		while (rem--)
+			reverse_rotate_stack(a, A);
+	}
+	else
+	{
+		rrr = var->ra;
+		rem = var->rb - rrr;
+		while (rrr--)
+			reverse_rotate_all_stack(a, b, ALL);
+		while (rem--)
+			reverse_rotate_stack(b, B);
+	}
+	a_to_b(var->rb, a, b);
+	b_to_a(var->ra, a, b);
+}
