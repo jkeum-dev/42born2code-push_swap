f7cb16e8230ccaa27b689b28f2eece5a59431fbe
JongeunKeum <jon9eun@gmail.com>
2021-05-29 21:48:24 +0900

    modify arg_three

--- srcs/sort_arg_three_a.c
+++ srcs/sort_arg_three_a.c
@@ -2,26 +2,63 @@
 
 static void	arg_three_top_min_a(t_stack *a, int max)
 {
-	if (a->top->next->value == max)
+	if (a->size == 3)
 	{
-		reverse_rotate_stack(a, A);
+		if (a->top->next->value == max)
+		{
+			reverse_rotate_stack(a, A);
+			swap_stack(a, A);
+		}
+	}
+	else
+	{
+		if (a->top->next->value == max)
+		{
+			rotate_stack(a, A);
+			swap_stack(a, A);
+			reverse_rotate_stack(a, A);
+		}
+	}
+}
+
+static void	arg_three_middle_min_a(t_stack *a, int max)
+{
+	if (a->size == 3)
+	{
+		if (a->bottom->value == max)
+			swap_stack(a, A);
+		else
+			rotate_stack(a, A);
+	}
+	else
+	{
 		swap_stack(a, A);
+		if (a->top->next->value == max)
+		{
+			rotate_stack(a, A);
+			swap_stack(a, A);
+			reverse_rotate_stack(a, A);
+		}
 	}
 }
 
 static void	arg_three_bottom_min_a(t_stack *a, int max)
 {
-	if (a->top->value == max)
-		swap_stack(a, A);
-	reverse_rotate_stack(a, A);
-}
-
-static void	arg_three_middle_min_a(t_stack *a, int max)
-{
-	if (a->bottom->value == max)
-		swap_stack(a, A);
+	if (a->size == 3)
+	{
+		if (a->top->value == max)
+			swap_stack(a, A);
+		reverse_rotate_stack(a, A);
+	}
 	else
+	{
+		if (a->top->value == max)
+			swap_stack(a, A);
 		rotate_stack(a, A);
+		swap_stack(a, A);
+		reverse_rotate_stack(a, A);
+		swap_stack(a, A);
+	}
 }
 
 void	handle_arg_three_a(int r, t_stack *a)
@@ -33,8 +70,8 @@ void	handle_arg_three_a(int r, t_stack *a)
 	max = get_max_value(a->top, r);
 	if (a->top->value == min)
 		arg_three_top_min_a(a, max);
-	else if (a->bottom->value == min)
-		arg_three_bottom_min_a(a, max);
 	else if (a->top->next->value == min)
 		arg_three_middle_min_a(a, max);
+	else if (a->top->next->next->value == min)
+		arg_three_bottom_min_a(a, max);
 }
--- srcs/sort_arg_three_b.c
+++ srcs/sort_arg_three_b.c
@@ -2,21 +2,44 @@
 
 void	arg_three_top_min_b(t_stack *b, int max)
 {
-	rotate_stack(b, B);
-	if (b->top->next->value == max)
+	if (b->size == 3)
+	{
+		rotate_stack(b, B);
+		if (b->top->next->value == max)
+			swap_stack(b, B);
+	}
+	else
+	{
 		swap_stack(b, B);
+		rotate_stack(b, B);
+		swap_stack(b, B);
+		reverse_rotate_stack(b, B);
+		if (b->top->next->value == max)
+			swap_stack(b, B);
+	}
+}
+
+void	arg_three_middle_min_b(t_stack *b, int max)
+{
+	if (b->size == 3)
+	{
+		reverse_rotate_stack(b, B);
+		if (b->top->next->value == max)
+			swap_stack(b, B);
+	}
+	else
+	{
+		rotate_stack(b, B);
+		swap_stack(b, B);
+		reverse_rotate_stack(b, B);
+		if (b->top->next->value == max)
+			swap_stack(b, B);
+	}
 }
 
 void	arg_three_bottom_min_b(t_stack *b, int max)
 {
 	if (b->top->next->value == max)
-		swap_stack(b, B);
-}
-
-void	arg_three_middle_min_b(t_stack *b, int max)
-{
-	reverse_rotate_stack(b, B);
-	if (b->top->value == max)
 		swap_stack(b, B);
 }
 
@@ -29,10 +52,10 @@ void	handle_arg_three_b(int r, t_stack *a, t_stack *b)
 	max = get_max_value(b->top, r);
 	if (b->top->value == min)
 		arg_three_top_min_b(b, max);
-	else if (b->bottom->value == min)
-		arg_three_bottom_min_b(b, max);
 	else if (b->top->next->value == min)
 		arg_three_middle_min_b(b, max);
+	else if (b->top->next->next->value == min)
+		arg_three_bottom_min_b(b, max);
 	push_stack(b, a, A);
 	push_stack(b, a, A);
 	push_stack(b, a, A);
