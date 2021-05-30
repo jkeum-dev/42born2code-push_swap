663eff3ae50bf6788a448239364872fe6e33f00c
JongeunKeum <jon9eun@gmail.com>
2021-05-30 22:25:38 +0900

    add arg only 5

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -21,8 +21,8 @@ void	select_pivot(int r, t_stack *stack, t_value *var)
 
 void	push_swap(t_stack *a, t_stack *b)
 {
-	int	r;
-
-	r = a->size;
-	a_to_b(r, a, b);
+	if (a->size == 5)
+		handle_arg_five(a, b);
+	else
+		a_to_b(a->size, a, b);
 }
