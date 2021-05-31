82b88a7103c66fd450decbeb7438e923b04598ae
JongeunKeum <jon9eun@gmail.com>
2021-05-31 21:31:35 +0900

    modify main

--- srcs/main.c
+++ srcs/main.c
@@ -36,49 +36,49 @@ int		is_sorted(t_stack *a)
 		return (0);
 }
 
-void	print_stack_a(t_stack *a)
-{
-	printf("\n\nstack a\n");
-	while (a->top)
-	{
-		printf("%d\t", a->top->value);
-		if (a->top->next)
-			a->top = a->top->next;
-		else
-			break ;
-	}
-	while (a->top->prev)
-		a->top = a->top->prev;
-	if (a->top && a->bottom)
-	{
-		printf("\ntop:\t%d\n", a->top->value);
-		printf("bottom:\t%d\n", a->bottom->value);
-		printf("size:\t%d\n", a->size);
-	}
-}
+// void	print_stack_a(t_stack *a)
+// {
+// 	printf("\n\nstack a\n");
+// 	while (a->top)
+// 	{
+// 		printf("%d\t", a->top->value);
+// 		if (a->top->next)
+// 			a->top = a->top->next;
+// 		else
+// 			break ;
+// 	}
+// 	while (a->top->prev)
+// 		a->top = a->top->prev;
+// 	if (a->top && a->bottom)
+// 	{
+// 		printf("\ntop:\t%d\n", a->top->value);
+// 		printf("bottom:\t%d\n", a->bottom->value);
+// 		printf("size:\t%d\n", a->size);
+// 	}
+// }
 
-void	print_stack_b(t_stack *b)
-{
-	printf("\n\nstack b\n");
-	if (!b->top)
-		return ;
-	while (b->top)
-	{
-		printf("%d\t", b->top->value);
-		if (b->top->next)
-			b->top = b->top->next;
-		else
-			break ;
-	}
-	while (b->top->prev)
-		b->top = b->top->prev;
-	if (b->top && b->bottom)
-	{
-		printf("\ntop:\t%d\n", b->top->value);
-		printf("bottom:\t%d\n", b->bottom->value);
-		printf("size:\t%d\n", b->size);
-	}
-}
+// void	print_stack_b(t_stack *b)
+// {
+// 	printf("\n\nstack b\n");
+// 	if (!b->top)
+// 		return ;
+// 	while (b->top)
+// 	{
+// 		printf("%d\t", b->top->value);
+// 		if (b->top->next)
+// 			b->top = b->top->next;
+// 		else
+// 			break ;
+// 	}
+// 	while (b->top->prev)
+// 		b->top = b->top->prev;
+// 	if (b->top && b->bottom)
+// 	{
+// 		printf("\ntop:\t%d\n", b->top->value);
+// 		printf("bottom:\t%d\n", b->bottom->value);
+// 		printf("size:\t%d\n", b->size);
+// 	}
+// }
 
 int	main(int argc, char *argv[])
 {
