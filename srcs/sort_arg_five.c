7d0a5af1d69563e090233e17b11792ee9f7d9d62
JongeunKeum <jon9eun@gmail.com>
2021-06-05 17:38:06 +0900

    norminette done

--- srcs/sort_arg_five.c
+++ srcs/sort_arg_five.c
@@ -1,95 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_arg_five.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/05 17:37:43 by jkeum             #+#    #+#             */
+/*   Updated: 2021/06/05 17:37:47 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int		get_mid_value_five(t_node *node)
+static void	sort_five_a(int size, t_stack *a, t_stack *b)
 {
-	int	i;
-	int	j;
-	int	tmp;
-	int	value[5];
-
-	i = -1;
-	while (++i < 5)
-	{
-		value[i] = node->value;
-		if (node->next)
-			node = node->next;
-		else
-			break ;
-	}
-	while (node->prev)
-		node = node->prev;
-	i = 5;
-	while (--i > 0)
-	{
-		j = -1;
-		while (++j < i)
-		{
-			if (value[j] > value[j + 1])
-			{
-				tmp = value[j + 1];
-				value[j + 1] = value[j];
-				value[j] = tmp;
-			}
-		}
-	}
-	return (value[2]);
-}
-
-void	hanlde_sort_five(int size, t_stack *a, t_stack *b, int flag)
-{
-	int		mid;
-	int		push;
-	int		rotate;
+	int	mid;
+	int	push;
+	int	rotate;
 
 	push = 0;
 	rotate = 0;
+	mid = get_mid_value_five(a->top);
+	while (size--)
+	{
+		if (a->top->value < mid)
+		{
+			push_stack(a, b, B);
+			push++;
+		}
+		else
+		{
+			rotate_stack(a, A);
+			rotate++;
+		}
+		if (push == 2)
+			break ;
+	}
+	while (rotate--)
+		reverse_rotate_stack(a, A);
+}
+
+static void	sort_five_b(int size, t_stack *a, t_stack *b)
+{
+	int	mid;
+	int	push;
+	int	rotate;
+
+	push = 0;
+	rotate = 0;
+	mid = get_mid_value_five(b->top);
+	while (size--)
+	{
+		if (b->top->value >= mid)
+		{
+			push_stack(b, a, A);
+			push++;
+		}
+		else
+		{
+			rotate_stack(b, B);
+			rotate++;
+		}
+		if (push == 3)
+			break ;
+	}
+	while (rotate--)
+		reverse_rotate_stack(b, B);
+}
+
+void		hanlde_sort_five(int size, t_stack *a, t_stack *b, int flag)
+{
 	if (flag == A)
-	{
-		mid = get_mid_value_five(a->top);
-		while (size--)
-		{
-			if (a->top->value < mid)
-			{
-				push_stack(a, b, B);
-				push++;
-			}
-			else
-			{
-				rotate_stack(a, A);
-				rotate++;
-			}
-			if (push == 2)
-				break ;
-		}
-		while (rotate--)
-			reverse_rotate_stack(a, A);
-	}
+		sort_five_a(size, a, b);
 	else
-	{
-		mid = get_mid_value_five(b->top);
-		while (size--)
-		{
-			if (b->top->value >= mid)
-			{
-				push_stack(b, a, A);
-				push++;
-			}
-			else
-			{
-				rotate_stack(b, B);
-				rotate++;
-			}
-			if (push == 3)
-				break ;
-		}
-		while (rotate--)
-			reverse_rotate_stack(b, B);
-	}
+		sort_five_b(size, a, b);
 	handle_arg_three_a(3, a);
 	handle_arg_two(a, b, B);
 }
 
-void	handle_arg_five(t_stack *a, t_stack *b)
+void		handle_arg_five(t_stack *a, t_stack *b)
 {
 	int	pb;
 	int	min;
