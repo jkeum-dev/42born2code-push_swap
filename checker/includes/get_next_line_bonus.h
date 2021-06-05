bce630b5352bb6e7f76d1ef5672c99bf5e4b63eb
JongeunKeum <jon9eun@gmail.com>
2021-06-05 19:26:19 +0900

    add gnl header file

---
+++ checker/includes/get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 21:58:51 by jkeum             #+#    #+#             */
+/*   Updated: 2021/06/05 19:21:39 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# define OPEN_MAX 1024
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+int		get_next_line(int fd, char **line);
+int		ft_strlen(char *str);
+char	*ft_strjoin_gnl(char *save, char *buff);
+
+#endif
