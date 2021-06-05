34745ade34f159e3615c53688f8270924139c96a
JongeunKeum <jon9eun@gmail.com>
2021-06-05 17:26:15 +0900

    norminette done

--- srcs/sort_a_to_b.c
+++ srcs/sort_a_to_b.c
@@ -1,77 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_a_to_b.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/05 17:23:47 by jkeum             #+#    #+#             */
+/*   Updated: 2021/06/05 17:23:48 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void	a_to_b(int r, t_stack *a, t_stack *b, int *cnt)
+static int	exceptional_cases(int r, t_stack *a, t_stack *b)
 {
-	int		r_temp;
-	int		rrr;
-	int		rem;
-	t_value	var;
-
 	if (r <= 3)
 	{
 		handle_under_three(r, a, b, A);
-		return ;
+		return (0);
 	}
-	if (r == 5)
+	else if (r == 5)
 	{
 		hanlde_sort_five(5, a, b, A);
-		return ;
+		return (0);
 	}
+	else
+		return (1);
+}
+
+static void	push_rotate_a(t_stack *a, t_stack *b, t_value *var)
+{
+	if (a->top->value > var->piv_big)
+	{
+		rotate_stack(a, A);
+		var->ra++;
+	}
+	else
+	{
+		push_stack(a, b, B);
+		var->pb++;
+		if (b->top->value > var->piv_small)
+		{
+			rotate_stack(b, B);
+			var->rb++;
+		}
+	}
+}
+
+static void	back_to_orig_ra(t_stack *a, t_stack *b, int *cnt, t_value *var)
+{
+	int	rrr;
+	int	rem;
+
+	rrr = var->rb;
+	rem = var->ra - rrr;
+	if ((*cnt) > 0)
+	{
+		while (rrr--)
+			reverse_rotate_all_stack(a, b, ALL);
+		while (rem--)
+			reverse_rotate_stack(a, A);
+	}
+	else
+	{
+		while (rrr--)
+			reverse_rotate_stack(b, B);
+	}
+}
+
+static void	back_to_orig_rb(t_stack *a, t_stack *b, int *cnt, t_value *var)
+{
+	int	rrr;
+	int	rem;
+
+	rrr = var->ra;
+	rem = var->rb - rrr;
+	if ((*cnt) > 0)
+	{
+		while (rrr--)
+			reverse_rotate_all_stack(a, b, ALL);
+		while (rem--)
+			reverse_rotate_stack(b, B);
+	}
+	else
+	{
+		while (rrr--)
+			reverse_rotate_stack(a, A);
+	}
+}
+
+void		a_to_b(int r, t_stack *a, t_stack *b, int *cnt)
+{
+	int		r_temp;
+	t_value	var;
+
+	if (!exceptional_cases(r, a, b))
+		return ;
 	init_value(&var);
 	select_pivot(r, a, &var);
 	r_temp = r;
 	while (r_temp--)
-	{
-		if (a->top->value > var.piv_big)
-		{
-			rotate_stack(a, A);
-			var.ra++;
-		}
-		else
-		{
-			push_stack(a, b, B);
-			var.pb++;
-			if (b->top->value > var.piv_small)
-			{
-				rotate_stack(b, B);
-				var.rb++;
-			}
-		}
-	}
+		push_rotate_a(a, b, &var);
 	if (var.ra > var.rb)
-	{
-		rrr = var.rb;
-		rem = var.ra - rrr;
-		if ((*cnt) > 0)
-		{
-			while (rrr--)
-				reverse_rotate_all_stack(a, b, ALL);
-			while (rem--)
-				reverse_rotate_stack(a, A);
-		}
-		else
-		{
-			while (rrr--)
-				reverse_rotate_stack(b, B);
-		}
-	}
+		back_to_orig_ra(a, b, cnt, &var);
 	else
-	{
-		rrr = var.ra;
-		rem = var.rb - rrr;
-		if ((*cnt) > 0)
-		{
-			while (rrr--)
-				reverse_rotate_all_stack(a, b, ALL);
-			while (rem--)
-				reverse_rotate_stack(b, B);
-		}
-		else
-		{
-			while (rrr--)
-				reverse_rotate_stack(a, A);
-		}
-	}
+		back_to_orig_rb(a, b, cnt, &var);
 	a_to_b(var.ra, a, b, cnt);
 	b_to_a(var.rb, a, b, cnt);
 	b_to_a(var.pb - var.rb, a, b, cnt);
