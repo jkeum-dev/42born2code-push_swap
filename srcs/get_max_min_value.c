e445ac187da1179f8d4d587baef3c3d13c62f9bc
JongeunKeum <jon9eun@gmail.com>
2021-06-05 17:37:25 +0900

    add functions, norminette done

--- srcs/get_max_min_value.c
+++ srcs/get_max_min_value.c
@@ -6,13 +6,55 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 16:37:16 by jkeum             #+#    #+#             */
-/*   Updated: 2021/06/05 16:37:17 by jkeum            ###   ########.fr       */
+/*   Updated: 2021/06/05 17:36:17 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	get_min_value(t_node *node, int size)
+static int	sort_values(int value[])
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 5;
+	while (--i > 0)
+	{
+		j = -1;
+		while (++j < i)
+		{
+			if (value[j] > value[j + 1])
+			{
+				tmp = value[j + 1];
+				value[j + 1] = value[j];
+				value[j] = tmp;
+			}
+		}
+	}
+	return (value[2]);
+}
+
+int			get_mid_value_five(t_node *node)
+{
+	int	i;
+	int	value[5];
+
+	i = -1;
+	while (++i < 5)
+	{
+		value[i] = node->value;
+		if (node->next)
+			node = node->next;
+		else
+			break ;
+	}
+	while (node->prev)
+		node = node->prev;
+	return (sort_values(value));
+}
+
+int			get_min_value(t_node *node, int size)
 {
 	int	min;
 
@@ -31,7 +73,7 @@ int	get_min_value(t_node *node, int size)
 	return (min);
 }
 
-int	get_max_value(t_node *node, int size)
+int			get_max_value(t_node *node, int size)
 {
 	int	max;
 
