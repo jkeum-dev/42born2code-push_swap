4cbc1436d0bde72a53bb87c2628b146b82c04e08
JongeunKeum <jon9eun@gmail.com>
2021-06-07 17:30:34 +0900

    main.c norminette done

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,18 @@
-#include "push_swap.h"
-// #include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 17:29:49 by jkeum             #+#    #+#             */
+/*   Updated: 2021/06/07 17:30:03 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int		is_sorted(t_stack *a)
+#include "push_swap.h"
+
+int	is_sorted(t_stack *a)
 {
 	t_node	*node;
 
@@ -24,50 +35,6 @@ int		is_sorted(t_stack *a)
 		return (0);
 }
 
-// void	print_stack_a(t_stack *a)
-// {
-// 	printf("\n\nstack a\n");
-// 	while (a->top)
-// 	{
-// 		printf("%d\t", a->top->value);
-// 		if (a->top->next)
-// 			a->top = a->top->next;
-// 		else
-// 			break ;
-// 	}
-// 	while (a->top->prev)
-// 		a->top = a->top->prev;
-// 	if (a->top && a->bottom)
-// 	{
-// 		printf("\ntop:\t%d\n", a->top->value);
-// 		printf("bottom:\t%d\n", a->bottom->value);
-// 		printf("size:\t%d\n", a->size);
-// 	}
-// }
-
-// void	print_stack_b(t_stack *b)
-// {
-// 	printf("\n\nstack b\n");
-// 	if (!b->top)
-// 		return ;
-// 	while (b->top)
-// 	{
-// 		printf("%d\t", b->top->value);
-// 		if (b->top->next)
-// 			b->top = b->top->next;
-// 		else
-// 			break ;
-// 	}
-// 	while (b->top->prev)
-// 		b->top = b->top->prev;
-// 	if (b->top && b->bottom)
-// 	{
-// 		printf("\ntop:\t%d\n", b->top->value);
-// 		printf("bottom:\t%d\n", b->bottom->value);
-// 		printf("size:\t%d\n", b->size);
-// 	}
-// }
-
 int	main(int argc, char *argv[])
 {
 	t_stack	*a;
@@ -82,20 +49,6 @@ int	main(int argc, char *argv[])
 		if (is_sorted(a))
 			return (0);
 		push_swap(a, b);
-		// print_stack_a(a);
-		// printf("top next: %d\n", a->top->next->value);
-		// printf("bottom prev: %d\n", a->bottom->prev->value);
-		// if (a->top->prev == NULL)
-		// 	printf("top prev : NULL\n");
-		// if (a->bottom->next == NULL)
-		// 	printf("bottom next : NULL\n");
-		// print_stack_b(b);
-		// printf("top next: %d\n", b->top->next->value);
-		// printf("bottom prev: %d\n", b->bottom->prev->value);
-		// if (b->top->prev == NULL)
-		// 	printf("top prev : NULL\n");
-		// if (b->bottom->next == NULL)
-		// 	printf("bottom next : NULL\n");
 		free_all(a, b);
 	}
 	return (0);
