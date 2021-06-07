712924921df5571239d99b128fb70ee2d82e7249
JongeunKeum <jon9eun@gmail.com>
2021-06-07 16:50:54 +0900

    do not print operations

--- checker/srcs/do_operations.c
+++ checker/srcs/do_operations.c
@@ -3,37 +3,37 @@
 void	do_push(char *buf, t_stack *a, t_stack *b)
 {
 	if (!ft_strcmp("pa", buf))
-		push_stack(b, a, A);
+		push_stack(b, a, 0);
 	else if (!ft_strcmp("pb", buf))
-		push_stack(a, b, B);
+		push_stack(a, b, 0);
 }
 
 void	do_swap(char *buf, t_stack *a, t_stack *b)
 {
 	if (!ft_strcmp("sa", buf))
-		swap_stack(a, A);
+		swap_stack(a, 0);
 	else if (!ft_strcmp("sb", buf))
-		swap_stack(b, B);
+		swap_stack(b, 0);
 	else
-		swap_all_stack(a, b, ALL);
+		swap_all_stack(a, b, 0);
 }
 
 void	do_rotate(char *buf, t_stack *a, t_stack *b)
 {
 	if (!ft_strcmp("ra", buf))
-		rotate_stack(a, A);
+		rotate_stack(a, 0);
 	else if (!ft_strcmp("rb", buf))
-		rotate_stack(b, B);
+		rotate_stack(b, 0);
 	else
-		rotate_all_stack(a, b, ALL);
+		rotate_all_stack(a, b, 0);
 }
 
 void	do_reverse_rotate(char *buf, t_stack *a, t_stack *b)
 {
 	if (!ft_strcmp("rra", buf))
-		reverse_rotate_stack(a, A);
+		reverse_rotate_stack(a, 0);
 	else if (!ft_strcmp("rrb", buf))
-		reverse_rotate_stack(b, B);
+		reverse_rotate_stack(b, 0);
 	else
-		reverse_rotate_all_stack(a, b, ALL);
+		reverse_rotate_all_stack(a, b, 0);
 }
